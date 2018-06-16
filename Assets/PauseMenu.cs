a259385ca3200cac8a97ad53a3cc3ad24b417c8d
BachNguyen1998 <ngb1998@gmail.com>
2018-06-17 02:46:19 +0700

    added Pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenuPanel;
+
+    public void Resume() {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
