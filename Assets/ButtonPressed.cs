7928155a331aae37b9258d90edffbce5c5d470e9
BachNguyen1998 <ngb1998@gmail.com>
2018-06-17 18:38:04 +0700

    added EscapeButton and WinningPanel

---
+++ Assets/ButtonPressed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressed : MonoBehaviour {
+    Animator anim;
+    public GameObject WinningPanel;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetBool("pressed",true);
+            SoundManagerScript.PlaySound("buttonPressed");
+            Time.timeScale = 0.3f;
+            Invoke("EnableWinning",2);
+        }    
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetBool("pressed", false);
+        }
+    }
+    void EnableWinning()
+    {
+        WinningPanel.SetActive(true);
+    }
+}
